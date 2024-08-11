3f7f05212b81214e54d3a20c13ebac92f23cef27
nitinmewar <nitinmewar28@gmail.com>
2024-08-12 03:37:30 +0530

    tcp connection

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello from new machine")
+	// listen for TCP connections
+	fmt.Println("listening on port http://localhost:6379")
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	for {
+		buf := make([]byte, 1024)
+
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("error while reading from client : ", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("mesage recieved : ", string(buf))
+
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
